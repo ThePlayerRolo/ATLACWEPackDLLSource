e98eddb4e2b77e58cd836cbce40b69dfedc596e3
ThePlayerRolo <thesolis78243@gmail.com>
2024-11-22 17:03:36 -0500

    New Items and balancing

--- main.cpp
+++ main.cpp
@@ -9,24 +9,32 @@ extern "C"
     void (*RegisterDataFunc)(void* ptr);
     //Lens function - Needed for CWE to recognize the lens.
     __declspec(dllexport) void(__cdecl* ALS_LensSpecial)(ObjectMaster*, ObjectMaster*);
-    NJS_TEXNAME ATLATEX[4];
+    NJS_TEXNAME ATLATEX[5];
     NJS_TEXLIST ATLATEXLIST = { arrayptrandlength(ATLATEX) };
     NJS_TEXNAME AVATARSTATELENSETEX[8];
     NJS_TEXLIST AVATARSTATELENSET_TEXLIST = { arrayptrandlength(AVATARSTATELENSETEX) };
-    BlackMarketItemAttributes BMAangHeadBand = { 250, 100, 0, -1, -1, 0 };
+    BlackMarketItemAttributes BMAangHeadBand = { 250, 100, 60, -1, -1, 0 };
     BlackMarketItemAttributes BMGliderOpen = { 1000, 250, 0, -1, -1, 0 };
-    BlackMarketItemAttributes BMSeaPrunes = { 350, 200, 0, -1, -1, 0 };
-    BlackMarketItemAttributes BMAvatarStateLense = { 10, 100, 0, -1, -1, 0 };;
+    BlackMarketItemAttributes BMGliderClosed = { 1000, 250, 0, -1, -1, 0 };
+    BlackMarketItemAttributes BMSeaPrunes = { 350, 200, 15, -1, -1, 0 };
+    BlackMarketItemAttributes BMAirNecklace = { 5000, 2000, 180, -1, -1, 0 };
+    BlackMarketItemAttributes BMBoomerang = { 3000, 500, 0, -1, -1, 0 };
+    BlackMarketItemAttributes BMAvatarStateLense = { 10, 100, 0, -1, -1, 0 };
     ModelInfo* MDLAvatarStateLense;
     ChaoItemStats SeaPruneStats = { 10, 0, 100, 0, 0, 0, 100, 0, 0, 0 };
     ModelInfo* MDLSeaPrunes;
     ModelInfo* MDLAangHeadBand;
     ModelInfo* MDLGliderOpen;
+    ModelInfo* MDLGliderClosed;
+    ModelInfo* MDLBoomerang;
+    ModelInfo* MDLAirNecklace;
     int AangHeadBandID;
     int ATLASeaPrunesID;
     int AvatarStateLenseID;
     int AangsGliderOpenID;
-
+    int AangsGliderClosedID;
+    int BoomerangID;
+    int AirNecklaceID;
 
     void __cdecl SeaPrunesFunction(ChaoData* data, ObjectMaster* fruit)
     {
@@ -37,6 +45,7 @@ extern "C"
     void CWELoad(CWE_REGAPI* cwe_api)
     {
         //al_eye_1
+        //AirNecklace.sa2mdl
         cwe_api->RegisterChaoTexlistLoad("HeadBandTextures", &ATLATEXLIST);
         cwe_api->RegisterChaoTexlistLoad("AvatarStateLense", &AVATARSTATELENSET_TEXLIST);
         AangHeadBandID = cwe_api->RegisterChaoAccessory(Head, MDLAangHeadBand->getmodel(), &ATLATEXLIST, &BMAangHeadBand, "The Headband", "\"Hotmen. Hotmen. Hotmen. Hotmen. Hotmen.\" \" Its over, we caught you! \" \" Who, me?\"");
@@ -45,9 +54,11 @@ extern "C"
         AangsGliderOpenID = cwe_api->RegisterChaoAccessory(Generic1, MDLGliderOpen->getmodel(), &ATLATEXLIST, &BMGliderOpen, "Aang's Glider", "One of the last things Aang has connecting him to his original people.");
         cwe_api->RegisterEyeColor("al_eye_1_avatar", &AVATARSTATELENSET_TEXLIST, AvatarStateLenseID);
         cwe_api->RegisterBlackMarketGeneralFruit(ATLASeaPrunesID, 50);
+        AangsGliderClosedID = cwe_api->RegisterChaoAccessory(Generic1, MDLGliderClosed->getmodel(), &ATLATEXLIST, &BMGliderClosed, "Aang's Glider Closed", "One of the last things Aang has connecting him to his original people.");
+        BoomerangID = cwe_api->RegisterChaoAccessory(Generic1, MDLBoomerang->getmodel(), &ATLATEXLIST, &BMBoomerang, "Boomerang", " \" Hey, I'm the guy who did the boomerang thing.\" \"YEAH, BOOMERANG!\"");
+        AirNecklaceID = cwe_api->RegisterChaoAccessory(Generic2, MDLAirNecklace->getmodel(), &ATLATEXLIST, &BMAirNecklace, "Air Necklace", "You made it, you stopped the war. The nations can live in harmony again.");
         
     }
-    //GliderOpen.sa2mdl
     //initialization function - MUST exist in order to have CWE and SA2 see your mod
     __declspec(dllexport) void Init(const char* path)
     {
@@ -59,6 +70,9 @@ extern "C"
         MDLSeaPrunes = new ModelInfo(pathStr + "SeaPrunes.sa2mdl");
         MDLAvatarStateLense = new ModelInfo(pathStr + "Avatar State Lense.sa2mdl");
         MDLGliderOpen = new ModelInfo(pathStr + "GliderOpen.sa2mdl");
+        MDLBoomerang = new ModelInfo(pathStr + "Boomerang.sa2mdl");
+        MDLGliderClosed = new ModelInfo(pathStr + "ClosedGlider.sa2mdl");
+        MDLAirNecklace = new ModelInfo(pathStr + "AirNecklace.sa2mdl");
         RegisterDataFunc = (void (*)(void* ptr))GetProcAddress(h, "RegisterDataFunc");
         RegisterDataFunc(CWELoad);
     }
